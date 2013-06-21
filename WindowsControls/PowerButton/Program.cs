18fa1d834ec35593412f219da5412852a67ebdd1
Patrick Toohey <ptoohey@oswego.edu>
2013-06-21 16:34:04 -0400

    replacement windows start button and power shortcut

---
+++ WindowsControls/PowerButton/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace PowerButton
+{
+    using System.Windows.Input;
+
+    using Microsoft.Test;
+
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Keyboard.Press(Key.LWin);
+            Keyboard.Press(Key.I);
+            Keyboard.Release(Key.I);
+            Keyboard.Release(Key.LWin);
+        }
+    }
+}
---
+++ WindowsControls/StartButton/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace StartButton
+{
+    using System.Windows.Input;
+
+    using Microsoft.Test;
+
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Keyboard.Press(Key.LWin);
+            Keyboard.Release(Key.LWin);
+        }
+    }
+}
