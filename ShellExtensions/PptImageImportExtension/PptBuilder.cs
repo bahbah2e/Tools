3bf9dafd52f9966f6dc270c75a03f8a282493573
Patrick Toohey <ptoohey@oswego.edu>
2013-06-26 08:14:02 -0400

    implementation of image compression

--- ShellExtensions/PptImageImportExtension/PptBuilder.cs
+++ ShellExtensions/PptImageImportExtension/PptBuilder.cs
@@ -1,12 +1,88 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace PptImageImportExtension
+﻿namespace PptImageImportExtension
 {
-    class PptBuilder
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Linq;
+
+    using Microsoft.Office.Core;
+    using Microsoft.Office.Interop.PowerPoint;
+
+    internal class PptBuilder
     {
+        private readonly Application ppt = new Application();
+
+        private readonly Presentation presentation;
+
+        private readonly Slides slides;
+
+        public PptBuilder()
+        {
+            Presentations presentations = this.ppt.Presentations;
+            this.presentation = presentations.Add();
+            this.presentation.PageSetup.SlideSize = PpSlideSizeType.ppSlideSizeOnScreen16x10;
+            this.slides = this.presentation.Slides;
+        }
+
+        public void AddToPpt(FileInfo[] unsortedPngFiles)
+        {
+            int pageNumber = 0;
+            FileInfo[] sortedPngFiles = this.Sort(unsortedPngFiles);
+
+            foreach (FileInfo capture in sortedPngFiles)
+            {
+                Console.WriteLine("Adding {0}", capture.Name);
+                _Slide step = this.slides.Add(++pageNumber, PpSlideLayout.ppLayoutBlank);
+
+                step.Shapes.AddPicture(
+                    string.Format(capture.FullName),
+                    MsoTriState.msoFalse,
+                    MsoTriState.msoTrue,
+                    1.4454f,
+                    1.0f,
+                    717f,
+                    448f);
+            }
+
+            if (sortedPngFiles.Length > 0)
+            {
+                string pngOne = sortedPngFiles[0].FullName.Replace("png", "ppt");
+                this.presentation.SaveAs(pngOne);
+            }
+        }
+
+        public FileInfo[] Sort(string directory)
+        {
+            var localDirectory = new DirectoryInfo(directory);
+            FileInfo[] pngFiles = localDirectory.GetFiles("*.png");
+            var sortedFiles = new Dictionary<int, FileInfo>();
+
+            foreach (FileInfo capture in localDirectory.GetFiles("*.png"))
+            {
+                int index = this.GetFileIndex(capture.Name);
+                sortedFiles.Add(index, capture);
+            }
+
+            IEnumerable<FileInfo> sortedPngFiles = sortedFiles.OrderBy(kv => kv.Key).Select(kv => kv.Value);
+            return sortedPngFiles as FileInfo[];
+        }
+
+        public FileInfo[] Sort(FileInfo[] unsorted)
+        {
+            var sortedFilesHash = new Dictionary<int, FileInfo>();
+
+            foreach (FileInfo capture in unsorted)
+            {
+                int index = this.GetFileIndex(capture.Name);
+                sortedFilesHash.Add(index, capture);
+            }
+            return sortedFilesHash.OrderBy(kv => kv.Key).Select(kv => kv.Value).ToArray();
+        }
+
+        private int GetFileIndex(string fileName)
+        {
+            int indexSeparator = fileName.IndexOf('.');
+            return Int32.Parse(fileName.Substring(0, indexSeparator));
+        }
     }
 }
--- ShellExtensions/PptImageImportExtension/PptBuilderContextMenu.cs
+++ ShellExtensions/PptImageImportExtension/PptBuilderContextMenu.cs
@@ -1,12 +1,62 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace PptImageImportExtension
+﻿namespace PptImageImportExtension
 {
-    public class Class1
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Runtime.InteropServices;
+
+    using ContextMenuExtension;
+
+    [Guid("A59463CA-C047-4C74-AC1F-D9E6E2BA2DD6")]
+    [ComVisible(true)]
+    public class PptBuilderContextMenu : ContextMenuExt
     {
+        private const string FriendlyName = "PNG To PPT";
+
+        public PptBuilderContextMenu()
+        {
+            this.MenuText = "&Add to Powerpoint";
+            this.Verb = "buildPPT";
+            this.VerbHelpText = "Add to Powerpoint";
+            this.VerbCanonicalName = "PptBuilder";
+            this.SupportedFileTypes.Add(".png");
+            this.SupportedFileTypes.Add(".jpg");
+        }
+
+        public override string RegistryFriendlyName
+        {
+            get { return FriendlyName; }
+            set { }
+        }
+
+        protected override string MenuText { get; set; }
+
+        protected override string Verb { get; set; }
+
+        protected override string VerbCanonicalName { get; set; }
+
+        protected override string VerbHelpText { get; set; }
+
+        protected override void OnVerb(IntPtr hWnd)
+        {
+            var files = new List<FileInfo>();
+
+            foreach (string file in this.SelectedFiles)
+            {
+                files.Add(new FileInfo(file));
+            }
+
+            var converter = new PptBuilder();
+            converter.AddToPpt(files.ToArray());
+        }
+
+        private IntPtr BuildBitmap()
+        {
+            // Load the bitmap for the menu item.
+            //Bitmap bmp = Resources.OK;
+            //bmp.MakeTransparent(bmp.GetPixel(0, 0));
+            //this.MenuBitmap = bmp.GetHbitmap();
+            return IntPtr.Zero;
+        }
     }
 }
